d8bb67d1126cd29b0b685b1a491eb1b5d48e347a
store-uc <storeucpubg@gmail.com>
2024-10-28 01:58:41 +0300

    script.js

--- script.js
+++ script.js
@@ -53,3 +53,18 @@ window.onclick = function(event) {
         }
     }
 }
+document.addEventListener('DOMContentLoaded', () => {
+    const faqQuestions = document.querySelectorAll('.faq-question');
+
+    faqQuestions.forEach((question) => {
+        question.addEventListener('click', () => {
+            question.classList.toggle('active');
+            const answer = question.nextElementSibling;
+            if (answer.style.display === "block") {
+                answer.style.display = "none";
+            } else {
+                answer.style.display = "block";
+            }
+        });
+    });
+});
